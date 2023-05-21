841839a11afc01dc9e3470faf0a59293a615d9e9
Sulakna Karunaratna <sulakna.karunaratna@arimaclanka.com>
2023-05-21 01:48:00 -0400

    characterSelectchanges

--- react-text-to-speech/src/components/CharacterCard.js
+++ react-text-to-speech/src/components/CharacterCard.js
@@ -2,24 +2,23 @@ import React from "react";
 import "../styles/CharacterCard.css";
 import { Droppable } from "react-beautiful-dnd";
 import RoleDraggable from "../components/RoleDraggable.js";
+import draganddropicon from "../Pictures/draganddropicon.png";
 
-
-
-function CharacterCard({ character, role  }) {
-
-
+function CharacterCard({ character, role }) {
   return (
     <div className="character-card">
-      <div className="card">
+      <div
+        className="card p-2 mb-5"
+        style={{ width: "200px", height: "150px" }}
+      >
         <div className="card-img-container">
           <img
-            src={character.img}
+            src={draganddropicon}
             className="card-img-top"
             alt={character.charater_name}
           />
         </div>
         <div className="card-body">
-          <h5 className="card-title">{character.charater_name}</h5>
           <Droppable droppableId={character.charater_name}>
             {(provided) => (
               <div
@@ -27,13 +26,20 @@ function CharacterCard({ character, role  }) {
                 ref={provided.innerRef}
                 className="droppable-area"
               >
-                {role && <RoleDraggable draggableId={role.Role} role={role} index={0} />}
-              {provided.placeholder}
+                {role && (
+                  <RoleDraggable
+                    draggableId={role.Role}
+                    role={role}
+                    index={0}
+                  />
+                )}
+                {provided.placeholder}
               </div>
             )}
           </Droppable>
         </div>
       </div>
+      <br></br>
     </div>
   );
 }
